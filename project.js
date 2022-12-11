5623bc33ce1a1a55b48befd0643a6be5b265333e
bilel <bilel.moudache0412@gmail.com>
2022-12-11 15:56:22 +0100

    fix bug in display turn and for display winner

--- project.js
+++ project.js
@@ -8,6 +8,7 @@ function player(name, mark) {
 const game = (() =>{
   let board = [];
   let roundNumber = 0;
+  let over;
 
   const domElement = (() => {
     const userInstruction = document.querySelector('.alert');
@@ -82,6 +83,7 @@ const game = (() =>{
     ) {
       domElement.finish.textContent = `${currentPlayer.name} WIN`;
       domElement.gameOver.style.cssText = 'visibility: visible;';
+      over = true;
     } else if (
       (((board[0] === board[3] && board[0] === board[6]) && (board[0] !== '')))
         || (((board[1] === board[4] && board[1] === board[7]) && (board[1] !== '')))
@@ -89,15 +91,18 @@ const game = (() =>{
     ) {
       domElement.finish.textContent = `${currentPlayer.name} WIN`;
       domElement.gameOver.style.cssText = 'visibility: visible;';
+      over = true;
     } else if (
       (((board[2] === board[4] && board[2] === board[6]) && (board[2] !== '')))
         || (((board[0] === board[4] && board[0] === board[8]) && (board[0] !== '')))
     ) {
       domElement.finish.textContent = `${currentPlayer.name} WIN`;
       domElement.gameOver.style.cssText = 'visibility: visible;';
+      over = true;
     } else if (roundNumber === 8) {
       domElement.finish.textContent = 'TIE';
       domElement.gameOver.style.cssText = 'visibility: visible;';
+      over = true;
     }
   };
 
@@ -136,97 +141,99 @@ const game = (() =>{
   const controlTheFlow = (name) => {
     checkWinner(name);
     roundNumber += 1;
-    displayTurn();
+    displayTurn(listenIaBtn());
   };
 
   const iaPlay = () => {
     const binaryBoard = board.filter(() => true);
     const isEmpty = (element) => element === '';
     // eslint-disable-next-line no-plusplus
-    for (let i = 0; i < 8; i++) {
+    for (let i = 0; i < 9; i++) {
       if (board[i] === 'X') {
-        binaryBoard[i] = 1;
+        binaryBoard[i] = 5;
       } else if (board[i] === 'O') {
-        binaryBoard[i] = 3;
+        binaryBoard[i] = 8;
+      } else if (board[i] === '') {
+        binaryBoard[i] = 1;
       }
     }
 
-    const firstRow = [binaryBoard[0] + binaryBoard[1] + binaryBoard[2]];
-    const secondRow = [binaryBoard[3] + binaryBoard[4] + binaryBoard[5]];
-    const thirdRow = [binaryBoard[6] + binaryBoard[7] + binaryBoard[8]];
+    const firstRow = [binaryBoard[0] * binaryBoard[1] * binaryBoard[2]];
+    const secondRow = [binaryBoard[3] * binaryBoard[4] * binaryBoard[5]];
+    const thirdRow = [binaryBoard[6] * binaryBoard[7] * binaryBoard[8]];
 
-    const firstColumn = [binaryBoard[0] + binaryBoard[3] + binaryBoard[6]];
-    const secondColumn = [binaryBoard[1] + binaryBoard[4] + binaryBoard[7]];
-    const thirdColumn = [binaryBoard[2] + binaryBoard[5] + binaryBoard[8]];
+    const firstColumn = [binaryBoard[0] * binaryBoard[3] * binaryBoard[6]];
+    const secondColumn = [binaryBoard[1] * binaryBoard[4] * binaryBoard[7]];
+    const thirdColumn = [binaryBoard[2] * binaryBoard[5] * binaryBoard[8]];
 
-    const firstDiagonal = [binaryBoard[0] + binaryBoard[4] + binaryBoard[8]];
-    const secondDiagonal = [binaryBoard[2] + binaryBoard[4] + binaryBoard[6]];
+    const firstDiagonal = [binaryBoard[0] * binaryBoard[4] * binaryBoard[8]];
+    const secondDiagonal = [binaryBoard[2] * binaryBoard[4] * binaryBoard[6]];
 
     if (roundNumber < 1) {
       board[board.findIndex(isEmpty)] = 'O';
     }
-    if (firstColumn[0] === '2') {
-      if (board[0] === '') {
+    if (firstColumn[0] === 25) {
+      if (binaryBoard[0] === 1) {
         board[0] = 'O';
-      } else if (board[3] === '') {
+      } else if (binaryBoard[3] === 1) {
         board[3] = 'O';
-      } else if (board[6] === '') {
+      } else if (binaryBoard[6] === 1) {
         board[6] = 'O';
       }
-    } else if (secondColumn[0] === '2') {
-      if (board[1] === '') {
+    } else if (secondColumn[0] === 25) {
+      if (binaryBoard[1] === 1) {
         board[1] = 'O';
-      } else if (board[4] === '') {
+      } else if (binaryBoard[4] === 1) {
         board[4] = 'O';
-      } else if (board[7] === '') {
+      } else if (binaryBoard[7] === 1) {
         board[7] = 'O';
       }
-    } else if (thirdColumn[0] === '2') {
-      if (board[2] === '') {
+    } else if (thirdColumn[0] === 25) {
+      if (binaryBoard[2] === 1) {
         board[2] = 'O';
-      } else if (board[5] === '') {
+      } else if (binaryBoard[5] === 1) {
         board[5] = 'O';
-      } else if (board[8] === '') {
+      } else if (binaryBoard[8] === 1) {
         board[8] = 'O';
       }
-    } else if (firstDiagonal[0] === '2') {
-      if (board[0] === '') {
+    } else if (firstDiagonal[0] === 25) {
+      if (binaryBoard[0] === 1) {
         board[0] = 'O';
-      } else if (board[4] === '') {
+      } else if (binaryBoard[4] === 1) {
         board[4] = 'O';
-      } else if (board[8] === '') {
+      } else if (binaryBoard[8] === 1) {
         board[8] = 'O';
       }
-    } else if (secondDiagonal[0] === '2') {
-      if (board[2] === '') {
+    } else if (secondDiagonal[0] === 25) {
+      if (binaryBoard[2] === 1) {
         board[2] = 'O';
-      } else if (board[4] === '') {
+      } else if (binaryBoard[4] === 1) {
         board[4] = 'O';
-      } else if (board[6] === '') {
+      } else if (binaryBoard[6] === 1) {
         board[6] = 'O';
       }
-    } else if (firstRow[0] === '2') {
-      if (board[0] === '') {
+    } else if (firstRow[0] === 25) {
+      if (binaryBoard[0] === 1) {
         board[0] = 'O';
-      } else if (board[1] === '') {
+      } else if (binaryBoard[1] === 1) {
         board[1] = 'O';
-      } else if (board[2] === '') {
+      } else if (binaryBoard[2] === 1) {
         board[2] = 'O';
       }
-    } else if (secondRow[0] === '2') {
-      if (board[3] === '') {
+    } else if (secondRow[0] === 25) {
+      if (binaryBoard[3] === 1) {
         board[3] = 'O';
-      } else if (board[4] === '') {
+      } else if (binaryBoard[4] === 1) {
         board[4] = 'O';
-      } else if (board[5] === '') {
+      } else if (binaryBoard[5] === 1) {
         board[5] = 'O';
       }
-    } else if (thirdRow[0] === '2') {
-      if (board[6] === '') {
+    } else if (thirdRow[0] === 25) {
+      if (binaryBoard[6] === 1) {
         board[6] = 'O';
-      } else if (board[7] === '') {
+      } else if (binaryBoard[7] === 1) {
         board[7] = 'O';
-      } else if (board[8] === '') {
+      } else if (binaryBoard[8] === 1) {
         board[8] = 'O';
       }
     } else if (roundNumber > 1) {
@@ -264,14 +271,18 @@ const game = (() =>{
           currentPlayer = displayTurn(listenIaBtn.getStatus());
           cell.innerHTML = `${currentPlayer.mark}`;
           updateBoard();
-          controlTheFlow(currentPlayer);
+          checkWinner(currentPlayer);
+          roundNumber += 1;
+          displayTurn(listenIaBtn.getStatus());
         } else if (cell.textContent === '' && listenIaBtn.getStatus() === true) {
           cell.innerHTML = `${'X'}`;
           updateBoard();
-          checkWinner(player1.name);
-          iaPlay();
-          checkWinner(player2.name);
-          roundNumber += 2;
+          checkWinner(player1);
+          if (over !== true) {
+            iaPlay();
+            checkWinner(player2);
+            roundNumber += 2;
+          }
         }
       });
     });
@@ -283,6 +294,7 @@ const game = (() =>{
       displayForm();
       resetBoard();
       updateBoard();
+      over = false;
       if (listenIaBtn.getStatus()) {
         domElement.off.style.cssText = 'visibility: hidden;';
         domElement.on.style.cssText = 'visibility: visible;';
